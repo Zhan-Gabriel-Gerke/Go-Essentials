f597d97ecd9e271e28d0acf2245cb31e66d8cf21
Zhan-Gabriel Gerke <93312041+Zhan-Gabriel-Gerke@users.noreply.github.com>
2025-12-04 18:36:08 +0200

    Pointers

---
+++ Pointers/pointers.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 32
+
+	var agePointer *int
+	agePointer = &age
+	fmt.Println("Age:", *agePointer)
+
+	//adultYears := editAgeToAdultYears(agePointer)
+	//fmt.Println(adultYears)
+	editAgeToAdultYears(agePointer)
+	fmt.Println(age)
+}
+
+func editAgeToAdultYears(age *int) {
+	//return *age - 18
+	*age = *age - 18
+}
